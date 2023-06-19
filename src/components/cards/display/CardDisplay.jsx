05c32b1ad86dfdffd63b2ea5e3a9bb902914438c
Marilia Carvalho <marilia.carvalho@delage.com.br>
2023-06-19 17:27:20 -0300

    cardDisplay finalizado

--- src/components/cards/display/CardDisplay.jsx
+++ src/components/cards/display/CardDisplay.jsx
@@ -1,27 +1,23 @@
 import { Image } from "../../Image/Image"
-import styles from "./cardDisplay.css";
+import styles from "./cardDisplay.module.css";
 
 export const CardDisplay = ({
-    imgWidth: width,
-    imgHeight: height,
     img,
     alt,
     title,
-    description
+    children
   }) => {
     return (
     <div className={styles.containerDisplay}>
-          <Image className={styles.containerImg}
-            alt={alt}
-            src={img}
-            width={width}
-            height={height}
-          />
-        
         <div className={styles.containerText}>
           <h1 className={styles.cardTitle}>{title}</h1>
-          <h2 className={styles.cardDescription}>{description}</h2>
+          <span className={styles.cardDescription}>{children}</span>
         </div>
+        <img className={styles.containerImg}
+          alt={alt}
+          src={img}
+        />
+        
     </div>
     );
   }
